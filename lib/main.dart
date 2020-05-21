2b31866f91703c4397de0f5285d06052ec84a208
Ezeh victor <ezehgvictor@gmail.com>
2020-05-22 00:31:16 +0100

    Add Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timer_app/widgets.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 void main()=> runApp(MyApp());
 
@@ -24,7 +25,10 @@ class TimerHomePage extends StatelessWidget{
     appBar:AppBar(
     title:Text("My work timer"),
       ),
-      body:Column(
+      body:
+       LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+      final double availableWidth = constraints.maxWidth;
+      return Column(
         children: <Widget>[
           Row(children: <Widget>[
             Padding(padding: EdgeInsets.all(8.0),),
@@ -42,9 +46,35 @@ class TimerHomePage extends StatelessWidget{
              text: "Long Break", onPressed: emptyMethod,  size: 23,)),
              Padding(padding: EdgeInsets.all(8.0),),
             
-          ],)
+          ],),
+
+                      Expanded(
+            child: CircularPercentIndicator(
+            radius: availableWidth / 2,
+            lineWidth: 10.0,
+            percent: 1,
+            center: Text("30:00",
+            style: Theme.of(context).textTheme.display1),
+            progressColor: Color(0xff009688),
+             ),
+            ),
+
+          
+          Row(children: [
+             Padding(padding: EdgeInsets.all(8.0),),
+             Expanded(child: ProductivityButton(color: Color(0xff212121),
+             text: 'Stop', onPressed: emptyMethod, size: 23,)),
+             Padding(padding: EdgeInsets.all(8.0),),
+             Expanded(child: ProductivityButton(color: Color(0xff009688),
+             text: 'Restart', onPressed: emptyMethod,size: 23,)),
+             Padding(padding: EdgeInsets.all(8.0),),
+        ],)
+
+
         ],
-      )
+      );
+      }
+    )
     );
   }
     void emptyMethod() {}
